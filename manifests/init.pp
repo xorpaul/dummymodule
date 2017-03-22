1945bdeac99b7cf05d1ed6eb503573d8e9d64b03
Andreas Paul <xorpaul@gmail.com>
2017-03-22 14:43:51 +0100

    init

---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+# == Class: dummymodule
+#
+# Full description of class dummymodule here
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*parameter1*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. 'Specify one or more upstream ntp servers as an array.'
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*variable1*]
+#   Explanation of how this variable affects the funtion of this class and
+#   if it has a default. e.g. 'The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames.'
+#
+# === Examples
+#
+#  class { 'dummymodule':
+#    parameter1 => [ 'just', 'an', 'example', ]
+#  }
+#
+# === Authors
+#
+# Andreas Paul <andreas.paul@1und1.de>
+#
+# === Copyright
+#
+# Copyright 2017-03-22 14:42 Andreas Paul
+#
+class dummymodule (){
+  
+}
+
+# vim: set ts=2 sta shiftwidth=2 softtabstop=2 expandtab foldmethod=syntax :
